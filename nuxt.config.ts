49999d79b615dc276ce5b3d56a726d7b388b3a18
zsh <zsh@qq.com>
2023-03-09 13:47:24 +0700

    更新favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 // 获取当前执行命令的自定义环境
-const ExcutingAnOrder = process.env.npm_lifecycle_script.split(' ');
+const ExcutingAnOrder = process.env.npm_lifecycle_script.split(" ");
 const env = ExcutingAnOrder[ExcutingAnOrder.length - 1];
 
 export default defineNuxtConfig({
   app: {
-    baseURL: '',
+    baseURL: "",
     head: {
-      title: 'dht',
-      titleTemplate: '%s - dht',
+      title: "zlbg",
+      titleTemplate: "%s - 最爱",
       // meta标签
       meta: [],
       // link方式引入的css或者文件
-      // link: [{ rel: 'icon', type: 'image/x-icon', href: '/zl/favicon.ico' }],
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "https://img.zlbg.cc/images/blog/icon-logo/extlbg.png",
+        },
+      ],
       // script文件
       // script: [{ src: '/zl/easemob.js', defer: true }],
     },
   },
-  modules: ['@element-plus/nuxt'],
+  modules: ["@element-plus/nuxt"],
   elementPlus: {
-    importStyle: 'css',
-    themes: ['dark'],
+    importStyle: "css",
+    themes: ["dark"],
     /** Options */
   },
 });
